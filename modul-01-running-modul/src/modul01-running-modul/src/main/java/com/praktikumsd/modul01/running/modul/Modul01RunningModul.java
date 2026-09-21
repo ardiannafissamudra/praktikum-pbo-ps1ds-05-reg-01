82e46509ed557e282fc7e6e4896bff974db03358
renwxyz <rendirendi11122@gmail.com>
2026-09-21 23:29:37 +0700

    feat: uji coba push project

---
+++ modul-01-running-modul/src/modul01-running-modul/src/main/java/com/praktikumsd/modul01/running/modul/Modul01RunningModul.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.praktikumsd.modul01.running.modul;
+
+/**
+ *
+ * @author Rendy
+ */
+public class Modul01RunningModul {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
