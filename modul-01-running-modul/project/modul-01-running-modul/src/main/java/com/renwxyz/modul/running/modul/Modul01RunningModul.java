cde4806a7687cf86062ed0cf0dae3abb057e2039
renwxyz <rendirendi11122@gmail.com>
2026-09-22 00:08:44 +0700

    Fix: .gitignore

---
+++ modul-01-running-modul/project/modul-01-running-modul/src/main/java/com/renwxyz/modul/running/modul/Modul01RunningModul.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.renwxyz.modul.running.modul;
+
+/**
+ *
+ * @author Rendy
+ */
+public class Modul01RunningModul {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
